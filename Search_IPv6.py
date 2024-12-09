7b0353fbebbe7320fb13b82cf3afd52ae7cbafdf
LabManOO1 <maxmaximmax37@gmail.com>
2024-12-09 21:00:46 +0300

    Тест методов поиска в файле и по веб-странице, интерфейс, комментарии.

--- Search_IPv6.py
+++ Search_IPv6.py
@@ -1,35 +1,59 @@
 import re
 import requests
-#import urllib
 
 
+class CheckIPv6:
+    """
+    CheckIPv6
+    Поиск IPv6 адресов в строке, в файле или по сайту
 
-class Check_IPv6:
+    pattern - регулярное выражение
+    """
     def __init__(self):
-        self.pattern = r"((?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4})"
+        self.pattern = r"((?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4})"  # Регулярное выражение
 
-    def check_string(self, string: str):
-        return re.findall(self.pattern, string)
+    def check_string(self, mystring: str):  # Поиск в строке
+        return re.findall(self.pattern, mystring)
 
-    def check_file(self):
+    def check_file(self):  # Поиск в файле
         try:
             with open("IPv6.txt", encoding="utf-8") as file:
-                string = file.read()
-                return self.check_string(string)
+                mystring = file.read()
+                return self.check_string(mystring)
         except Exception as error:
             return print(f"Ошибка при открытия файла: {error}")
 
-    def check_url(self):
+    def check_website(self):  # Поиск по веб-странице
         try:
             website = requests.get("https://habr.com/ru/articles/773708/")
             website.raise_for_status()
-            print(website.text)
             return self.check_string(website.text)
         except Exception as error:
             return f"Ошибка чтения страницы: {error}"
 
 
-
 if __name__ == "__main__":
-    a = Check_IPv6()
-    a.check_url()
+    value = 1
+    while value != 0:  # Интерфейс
+        search = CheckIPv6()
+        value = int(input("Выберете действие:\n1 - Поиск по строке\n2 - Поиск по файлу\n3 - Поиск по веб-странице\n0"
+                          " - Выход\n"))
+        if value == 1:
+            line = input("Введите строку:\t")
+            result = search.check_string(line)
+            if not result:
+                print("Нет совпадений!")
+            else:
+                print(result)
+        if value == 2:
+            result = search.check_file()
+            if not result:
+                print("Нет совпадений!")
+            else:
+                print(result)
+        if value == 3:
+            result = search.check_website()
+            if not result:
+                print("Нет совпадений!")
+            else:
+                print(result)
--- test_Search_IPv6.py
+++ test_Search_IPv6.py
@@ -1,19 +1,40 @@
 import unittest
-from Search_IPv6 import Check_IPv6
+from Search_IPv6 import CheckIPv6
 
 
-class TestCheckAll(unittest.TestCase):
+class TestSearchIPv6(unittest.TestCase):
+    """
+    TestSearchIPv6
+    Класс для проверки методов класса SearchIPv6
+    """
 
-    def setUp(self):
+    def setUp(self):  # Конфигурация тестов
         """Setup данных"""
-        self.search = Check_IPv6()
-        self.data = "0:0:0:0:0:0:0:0 AB12:BC5:0:99:67:FF0:F:BCA bb55:BC5:120:99:0:FAB:f:BCaF 0:0:0:0:0:0:0 Z:BC5:0:99:67:FF0:F:BCA 1:1:1:11111:1:1:1:1"
+        self.search = CheckIPv6()
+        self.data = ("0:0:0:0:0:0:0:0 AB12:BC5:0:99:67:FF0:F:BCA bb55:BC5:120:99:0:FAB:f:BCaF 0:0:0:0:0:0:0 Z:BC5:0:99:"
+                     "67:FF0:F:BCA 1:1:1:11111:1:1:1:1")
         self.correct_data = ["0:0:0:0:0:0:0:0", "AB12:BC5:0:99:67:FF0:F:BCA", "bb55:BC5:120:99:0:FAB:f:BCaF"]
 
-    def test_check_string(self):
+    def test_check_string(self):  # Проверка метода поиска в строке (check_string)
         """Тест функции проверки строки на валидных данных"""
 
-        self.assertEqual(self.search.check_string(string=self.data), self.correct_data)
+        self.assertEqual(self.search.check_string(mystring=self.data), self.correct_data)
+
+    def test_check_file(self):  # Проверка метода поиска в файле (check_file)
+        """Тест функции проверки строки на тестовом файле"""
+        self.assertEqual(self.search.check_file(), ['54:12:1:534:98:434:54:12', '11Ff:12a:1B:534:98C:434:54:12'])
+
+    def test_check_website(self):  # Проверка метода поиска на сайте (check_website)
+        """Тест функции проверки сайта на тестовом html-коде"""
+
+        self.assertEqual(self.search.check_website(),
+                         ['2001:DB8:0:0:0:0:0:1', '2001:0db8:2023:0910:60e1:76bd:601e:fa2f',
+                          '2001:0db8:2023:0910:36e1:7563:52e3:9043', '2001:0db8:2023:0910:baa2:45c8:8915:6d10',
+                          '2001:0db8:2023:0910:baa2:45c8:8915:6d10', '2001:0db8:2023:0910:60e1:76bd:601e:fa2f',
+                          '2001:0db8:2023:0910:36e1:7563:52e3:9043', '2001:DB8:0:0:0:0:0:1',
+                          '2001:0db8:2023:0910:60e1:76bd:601e:fa2f', '2001:0db8:2023:0910:36e1:7563:52e3:9043',
+                          '2001:0db8:2023:0910:baa2:45c8:8915:6d10', '2001:0db8:2023:0910:baa2:45c8:8915:6d10',
+                          '2001:0db8:2023:0910:60e1:76bd:601e:fa2f', '2001:0db8:2023:0910:36e1:7563:52e3:9043'])
 
 
 if __name__ == "__main__":
